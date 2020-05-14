c371c9424195ad42ee329f662dda45752f5beaca
https://github.com/ayomidebajo <oluwashinabajo@gmail.com>
2020-05-14 07:38:09 +0100

    Add flip-image-component with automation

--- src/App.js
+++ src/App.js
@@ -1,26 +1,86 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+// import classNames from 'classnames';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import "./App.css";
+
+class App extends Component {
+  componentDidMount() {
+    setInterval(this.picChanger, 5000);
+  }
+  state = {
+    ispressed: false,
+    pics: [
+      "https://picsum.photos/id/237/800/350",
+      "https://picsum.photos/id/1001/800/350",
+      "https://picsum.photos/id/1010/800/350",
+      "https://picsum.photos/id/1018/800/350",
+    ],
+    currentImage: 0,
+  };
+
+  onClick = (e) => {
+    // for (let i = 0; i < this.state.pics.length; i++) {
+    //   if 
+    // }
+
+   console.log('clicked');
+   this.setState({
+     isPressed: true
+   })
+   
+  };
+
+  // onClickPrev = (e) => {
+  //   if (this.state.currentImage < this.state.pics.length + 1) {
+  //     this.setState({
+  //       currentImage: this.state.currentImage - 1
+  //     });
+  //   } else {
+  //     this.setState({
+  //       currentImage:0
+  //     })
+  //   }
+  //   return this.currentImage
+
+  // }
+
+  picChanger = () => {
+    if (this.state.currentImage < this.state.pics.length - 1) {
+      this.setState({
+        currentImage: this.state.currentImage + 1,
+      });
+    } else {
+      this.setState({
+        currentImage: 0,
+      });
+    }
+    return this.currentImage;
+  };
+
+  render() {
+    return (
+      <div className="app--container">
+        <div className="image--container">
+          <div className="img">
+            <img src={this.state.pics[this.state.currentImage]} alt="random" />
+          </div>
+          <div className="dots">
+            <span onClick={this.onClick} className="dot">1</span>
+            <span onClick={this.onClick} className="dot">2</span>
+            <span onClick={this.onClick} className="dot">3</span>
+            <span onClick={this.onClick} className="dot">4</span>
+          </div>
+          {/* <a href="#" onClick={this.onClickPrev} className='fade prev'>
+              &#10094;
+            </a>
+
+            <a href="#" onClick={this.onClickNext} className='fade next'>
+            &#10095; 
+            </a> */}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
